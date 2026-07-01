6ae05f1b26f552fa1936a2d868c6d80ab7dfe610
Gino <gino.vanhoudt@lab900.com>
2026-07-01 08:17:39 +0200

    update Angular to v22

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withXhr,
+} from '@angular/common/http';
 import { delayInterceptor } from '../app/helpers/delay.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([delayInterceptor])),
+    provideHttpClient(withXhr(), withInterceptors([delayInterceptor])),
   ],
 };
